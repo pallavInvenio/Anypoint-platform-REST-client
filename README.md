eabdbf34d833f760ee186f5b7bd92e2c6241b04d
Pallav Kumar Sharma <pallav.kumar@inveniolsi.com>
2022-02-01 22:09:38 +0530

    Externalization of properties and App. part done

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Anypoint-platform-REST-client
- A rest client to work upon exposed api endpoints of Anypoint Platform
+
+A rest client to work upon exposed api endpoints of Anypoint Platform
+
+Kindly go through the property file for environment details.
--- entry_point.py
+++ entry_point.py
@@ -1,26 +1,27 @@
-# This is for accessing Anypoint platform - Runtime manager and cloudhub api
+# This is for accessing Anypoint platform - Runtime manager API
 
 import requests
-
-base_url = 'https://anypoint.mulesoft.com'
+import property_reader as properties
+from applications import view_applications
+from applications import view_application
+from applications import delete_application
+from applications import add_application
+from applications import update_application
 
 # for login using username and password
 
 
 def fetch_access_token(username, password):
-    login_path = base_url+'/accounts/login'
+    login_path = properties.get('base_url') + '/accounts/login'
     post_data = {"username": username, "password": password}
     r = requests.post(url=login_path, data=post_data)
     if r.status_code == 200:
-        print("Login successful")
         return r.json()['access_token']
-    else:
-        print("Incorrect username or password.")
-        return
+    return
 
 
 def fetch_organization_id(authorization_header):
-    path = base_url+'/accounts/api/profile'
+    path = properties.get('base_url') + '/accounts/api/profile'
     headers_dict = {"Authorization": authorization_header}
     r = requests.get(url=path, headers=headers_dict)
     if r.status_code == 200:
@@ -29,34 +30,35 @@ def fetch_organization_id(authorization_header):
 
 
 def fetch_environment_id(authorization_header):
-    path = base_url+'/accounts/api/profile'
+    path = properties.get('base_url') + '/accounts/api/profile'
     headers_dict = {"Authorization": authorization_header}
     r = requests.get(url=path, headers=headers_dict)
     if r.status_code == 200:
-        return r.json()['organization']['environments'][1]['id']
-    return
-
-
-def fetch_applications(headers_dict):
-    path = base_url+'/hybrid/api/v1/applications'
-    headers_dict = headers_dict
-    r = requests.get(url=path, headers=headers_dict)
-    if r.status_code == 200:
-        return r.json()
+        return r.json()['organization']['environments'][2]['id']
     return
 
 
 if __name__ == '__main__':
-    username = "m_daoud"  # externalize and secure
-    password = "Invenio2021"  # externalize and secure
-    authorization_header = 'Bearer ' + fetch_access_token(username, password)
+
+    # do login
+    access_token = fetch_access_token(
+        properties.get('username'), properties.get('password'))
+    if access_token:
+        print("Login successful")
+    else:
+        print("Incorrect username or password.")
+
+    # fetching header components
+    authorization_header = 'Bearer ' + access_token
     ORG_ID = fetch_organization_id(authorization_header)
     ENV_ID = fetch_environment_id(authorization_header)
-    headers_dict = {
-        "authorization": authorization_header,
-        "X-ANYPNT-ORG-ID": ORG_ID,
-        "X-ANYPNT-ENV-ID": ENV_ID
-    }
-    applications_list = fetch_applications(headers_dict)
 
-    print(applications_list)
+    # to get applications list
+    # applications_list = view_applications.get_applications(
+    #     authorization_header, ORG_ID, ENV_ID)
+    # print(applications_list)
+
+    # to get application detail based upon application id
+    #
+
+    # to
